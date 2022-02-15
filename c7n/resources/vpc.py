4857d3f4f064aafa85a7491b5a8704cd311da8dd
Kapil Thangavelu <kapilt@gmail.com>
2022-02-15 08:47:52 -0500

    fix permission typo

--- c7n/resources/vpc.py
+++ c7n/resources/vpc.py
@@ -2612,7 +2612,7 @@ class SubnetModifyAtrributes(BaseAction):
         }
     )
 
-    permissions = ("ec2:ModifySubentAttributes",)
+    permissions = ("ec2:ModifySubnetAttributes",)
 
     def process(self, resources):
         client = local_session(self.manager.session_factory).client('ec2')
