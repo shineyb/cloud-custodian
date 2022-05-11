91006517db36ea26beef935142603f08b25b3a5c
shiney bhayana <shiney.bhayana@capitalone.com>
2022-05-11 15:53:03 -0400

    fixed linting isssue
    removed config error
    
    added name
    
    Fixed linting issue

--- c7n/resources/lakeformation.py
+++ c7n/resources/lakeformation.py
@@ -1,9 +1,7 @@
-from c7n.actions import BaseAction
 from c7n.manager import resources
 from c7n import query
-from c7n.query import QueryResourceManager, DescribeSource, ConfigSource, TypeInfo
+from c7n.query import QueryResourceManager, ConfigSource, TypeInfo
 from c7n.tags import universal_augment
-
 
 
 class DescribeResource(query.DescribeSource):
@@ -12,21 +10,21 @@ class DescribeResource(query.DescribeSource):
             self.manager,
             super(DescribeResource, self).augment(resources))
 
+
 @resources.register('lakeformation')
 class LakeFormation(QueryResourceManager):
 
     class resource_type(TypeInfo):
         service = 'lakeformation'
-        enum_spec = ('list_resources', 'ResourceInfoList[].ResourceArn',None)
+        enum_spec = ('list_resources', 'ResourceInfoList[].ResourceArn', None)
         arn = id = 'ResourceArn'
-        detail_spec = ("describe_resource", "ResourceArn",None,"ResourceInfo")
-        cfn_type = config_type = "AWS::LakeFormation::Resource"
+        name = 'name'
+        detail_spec = ("describe_resource", "ResourceArn", None, "ResourceInfo")
+        cfn_type = "AWS::LakeFormation::Resource"
         arn_type = 'Resource'
         universal_taggable = object()
-
 
     source_mapping = {
         'describe': DescribeResource,
         'config': ConfigSource
     }
-
--- tests/test_lakeformation.py
+++ tests/test_lakeformation.py
@@ -1,18 +1,17 @@
 from .common import BaseTest
 
+
 class LakeFormationTest(BaseTest):
 
     def test_lakeformation_value_filter(self):
-        factory = self.record_flight_data("test_lakeformation_query_resources")
+        factory = self.replay_flight_data("test_lakeformation_query_resources")
         p = self.load_policy({
             'name': 'list_lakeformation_resources',
             'resource': 'lakeformation',
-            "filters": [{"RoleArn": "present"}],},
+            "filters": [{"RoleArn": "present"}], },
             session_factory=factory)
         resources = p.run()
         self.assertEqual(len(resources), 2)
-        TagsList=resources[0]['Tags']
+        TagsList = resources[0]['Tags']
         self.assertEqual((TagsList[0])['Key'], 'ResourceCreator')
         self.assertEqual((TagsList[0])['Value'], 'kapil')
-
-
