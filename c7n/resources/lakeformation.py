9c1bf62347b60211ab63b658938d817f276370ea
shiney bhayana <shiney.bhayana@capitalone.com>
2022-05-19 19:38:56 -0400

    Removed describe call

--- c7n/resources/lakeformation.py
+++ c7n/resources/lakeformation.py
@@ -1,14 +1,5 @@
 from c7n.manager import resources
-from c7n import query
-from c7n.query import QueryResourceManager, ConfigSource, TypeInfo
-from c7n.tags import universal_augment
-
-
-class DescribeResource(query.DescribeSource):
-    def augment(self, resources):
-        return universal_augment(
-            self.manager,
-            super(DescribeResource, self).augment(resources))
+from c7n.query import QueryResourceManager, TypeInfo
 
 
 @resources.register('lakeformation')
@@ -16,15 +7,9 @@ class LakeFormation(QueryResourceManager):
 
     class resource_type(TypeInfo):
         service = 'lakeformation'
-        enum_spec = ('list_resources', 'ResourceInfoList[].ResourceArn', None)
+        enum_spec = ('list_resources', 'ResourceInfoList', None)
         arn = id = 'ResourceArn'
         name = 'name'
-        detail_spec = ("describe_resource", "ResourceArn", None, "ResourceInfo")
         cfn_type = "AWS::LakeFormation::Resource"
         arn_type = 'Resource'
         universal_taggable = object()
-
-    source_mapping = {
-        'describe': DescribeResource,
-        'config': ConfigSource
-    }
--- tests/test_lakeformation.py
+++ tests/test_lakeformation.py
@@ -4,14 +4,12 @@ from .common import BaseTest
 class LakeFormationTest(BaseTest):
 
     def test_lakeformation_value_filter(self):
-        factory = self.replay_flight_data("test_lakeformation_query_resources")
+        factory = self.record_flight_data("test_lakeformation_query")
         p = self.load_policy({
             'name': 'list_lakeformation_resources',
             'resource': 'lakeformation',
             "filters": [{"RoleArn": "present"}], },
             session_factory=factory)
         resources = p.run()
+        print(resources)
         self.assertEqual(len(resources), 2)
-        TagsList = resources[0]['Tags']
-        self.assertEqual((TagsList[0])['Key'], 'ResourceCreator')
-        self.assertEqual((TagsList[0])['Value'], 'kapil')
