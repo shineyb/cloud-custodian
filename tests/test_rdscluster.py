1be76da7664355443ec332aaa4bc0f6b1943a9b1
Shiney Bhayana <shiney.bhayana@capitalone.com>
2022-08-25 14:59:39 -0400

    recorded api calls again for clarity

--- tests/test_rdscluster.py
+++ tests/test_rdscluster.py
@@ -123,7 +123,7 @@ class RDSClusterTest(BaseTest):
         self.assertEqual(len(resources), 1)
 
     def test_rdscluster_kms_alias(self):
-        session_factory = self.replay_flight_data("test_rdscluster_kms_alias", region="us-east-2")
+        session_factory = self.record_flight_data("test_rdscluster_kms_alias", region="us-east-2")
         p = self.load_policy(
             {
                 "name": "rds-cluster-aws-kms-key-filter",
